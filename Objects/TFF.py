d2a909ea76430d91d18ee96b006b5431b48528a0
Geojjj <georh122@gmail.com>
2022-09-16 21:15:04 +0300

    Added TFF

--- Objects/TFF.py
+++ Objects/TFF.py
@@ -24,12 +24,12 @@ class TFF(ComplexObjectBase):
         self.add_JJ(name='J3', c=1, r=1, A=1.5, B=0, loc=[sk[6], 0])
 
         self.add_L(name='L0', val=1, loc=[sk[0], sk[3]])
-        self.add_L(name='L1', val=1, loc=[sk[4], sk[5]])
-        self.add_L(name='L2', val=1, loc=[sk[5], sk[6]])
+        self.add_L(name='L1', val=2, loc=[sk[4], sk[5]])
+        self.add_L(name='L2', val=2, loc=[sk[5], sk[6]])
 
         self.add_L(name='L3', val=1, loc=[sk[4], sk[1]])
         self.add_L(name='L4', val=1, loc=[sk[6], sk[2]])
 
-        self.add_ib(name='Ib1', val=0.75, loc=[sk[4]])
+        self.add_ib(name='Ib1', val=1.45, loc=[sk[4]])
 
         self.add_R(name='R1', r=1, loc=[sk[5], 0])
