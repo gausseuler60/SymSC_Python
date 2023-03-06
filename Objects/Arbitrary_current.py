660896a7589e38dd05bc7e4dae553c67a3425ca0
Ivan Nazhestkin <61228847+gausseuler60@users.noreply.github.com>
2023-03-06 16:57:31 +0300

    Add arbitrary current source

---
+++ Objects/Arbitrary_current.py
@@ -0,0 +1,27 @@
+from Objects.CurrentSourceBase import CurrentSourceBase
+from Functions import sin_I
+import numpy as np
+
+
+class Arbitrary_current(CurrentSourceBase):
+    """
+    Arbitrary current I(t)
+    """
+    def __init__(self, loc, t, curr_t):
+        """
+        A class constructor
+
+        :param loc: nodes in a circuit to what the element is connected (zero is a ground)
+        :param t: simuation time array
+        :param curr_t: I(t) array (must be the same length as t)
+        """
+        super().__init__(loc=loc)
+
+        self.t = np.array(t)
+        self.curr_t = np.array(curr_t)
+
+        self.name = "Arbitrary_current"
+
+    def get_current_from_time(self, t):
+        idx = np.nonzero(t == self.t)[0][0]
+        return self.curr_t[idx]
